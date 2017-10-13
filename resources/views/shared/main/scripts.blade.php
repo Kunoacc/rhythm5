5b23622156994866cfa1d78deeeb03e4bb66f00c
Nelson Atuonwu <nellyatuonwu@gmail.com>
2017-10-13 13:31:03 +0100

    Added Assets

--- resources/views/shared/main/scripts.blade.php
+++ resources/views/shared/main/scripts.blade.php
@@ -1,35 +1,35 @@
-<script type="text/javascript" src="js/jquery/jquery.js"></script>
-<script type="text/javascript" src="js/jquery/jquery-migrate.min.js"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-migrate/3.0.1/jquery-migrate.min.jsjs/jquery/jquery-migrate.min.js"></script>
 
-<script type="text/javascript" src="js/vendor/photostack/modernizr.min.js"></script>
-<script type="text/javascript" src="js/_main.min.js"></script>
-<script type="text/javascript" src="js/_packed.js"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js"></script>
+<script type="text/javascript" src="{{asset('js/_main.min.js')}}"></script>
+<script type="text/javascript" src="{{asset('js/_packed.js')}}"></script>
 
-<script type="text/javascript" src="js/vendor/essential-grid/js/lightbox.js"></script>
-<script type="text/javascript" src="js/vendor/essential-grid/js/jquery.themepunch.tools.min.js"></script>
-<script type="text/javascript" src="js/vendor/essential-grid/js/jquery.themepunch.essential.min.js"></script>
-<script type="text/javascript" src="js/essential-grid-homepage-2.min.js"></script>
+<script type="text/javascript" src="{{asset('js/vendor/essential-grid/js/lightbox.js')}}"></script>
+<script type="text/javascript" src="{{asset('js/vendor/essential-grid/js/jquery.themepunch.tools.min.js')}}"></script>
+<script type="text/javascript" src="{{asset('js/vendor/essential-grid/js/jquery.themepunch.essential.min.js')}}"></script>
+<script type="text/javascript" src="{{asset('js/essential-grid-homepage-2.min.js')}}"></script>
 
-<script type="text/javascript" src="js/vendor/revslider/jquery.themepunch.revolution.min.js"></script>
+<script type="text/javascript" src="{{asset('js/vendor/revslider/jquery.themepunch.revolution.min.js')}}"></script>
 
-<script type="text/javascript" src="js/vendor/revslider/revolution.extension.slideanims.min.js"></script>
-<script type="text/javascript" src="js/vendor/revslider/revolution.extension.layeranimation.min.js"></script>
-<script type="text/javascript" src="js/vendor/revslider/revolution.extension.kenburn.min.js"></script>
-<script type="text/javascript" src="js/vendor/revslider/revolution.extension.navigation.min.js"></script>
-<script type="text/javascript" src="js/vendor/revslider/revolution.extension.parallax.min.js"></script>
-<script type="text/javascript" src="js/revslider-homepage-2.min.js"></script>
+<script type="text/javascript" src="{{asset('js/vendor/revslider/revolution.extension.slideanims.min.js')}}"></script>
+<script type="text/javascript" src="{{asset('js/vendor/revslider/revolution.extension.layeranimation.min.js')}}"></script>
+<script type="text/javascript" src="{{asset('js/vendor/revslider/revolution.extension.kenburn.min.js')}}"></script>
+<script type="text/javascript" src="{{asset('js/vendor/revslider/revolution.extension.navigation.min.js')}}"></script>
+<script type="text/javascript" src="{{asset('js/vendor/revslider/revolution.extension.parallax.min.js')}}"></script>
+<script type="text/javascript" src="{{asset('js/revslider-homepage-2.min.js')}}"></script>
 
-<script type="text/javascript" src="js/superfish.min.js"></script>
-<script type="text/javascript" src="js/core.utils.min.js"></script>
-<script type="text/javascript" src="js/core.init.min.js"></script>
-<script type="text/javascript" src="js/template.init.min.js"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/superfish/1.7.9/js/superfish.min.js"></script>
+<script type="text/javascript" src="{{asset('js/core.utils.min.js')}}"></script>
+<script type="text/javascript" src="{{asset('js/core.init.min.js')}}"></script>
+<script type="text/javascript" src="{{asset('js/template.init.min.js')}}"></script>
 
-<script type="text/javascript" src="js/shortcodes.min.js"></script>
-<script type="text/javascript" src="js/core.messages.min.js"></script>
-<script type="text/javascript" src="js/vendor/magnific/jquery.magnific-popup.min.js"></script>
+<script type="text/javascript" src="{{asset('js/shortcodes.min.js')}}"></script>
+<script type="text/javascript" src="{{asset('js/core.messages.min.js')}}"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/jquery.magnific-popup.min.js"></script>
 
-<script type="text/javascript" src="js/countdown/jquery.plugin.min.js"></script>
-<script type="text/javascript" src="js/countdown/jquery.countdown.min.js"></script>
+<script type="text/javascript" src="{{asset('js/countdown/jquery.plugin.min.js')}}"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery.countdown/2.2.0/jquery.countdown.min.js"></script>
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.0.1/js/swiper.min.js"></script>
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery.isotope/3.0.4/isotope.pkgd.min.js"></script>
-<script type="text/javascript" src="js/jquery/ui/accordion.min.js"></script>
+<script type="text/javascript" src="{{asset('js/jquery/ui/accordion.min.js')}}"></script>
