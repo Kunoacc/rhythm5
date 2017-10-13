c2d43c6fa75d4dfba4786dbf4131145ee574ff33
Nelson Atuonwu <nellyatuonwu@gmail.com>
2017-10-13 13:13:46 +0100

    Initial Commit

---
+++ resources/views/shared/main/head.blade.php
@@ -0,0 +1,34 @@
+<head>
+    <meta charset="UTF-8" />
+    <!--<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">-->
+    <meta name="format-detection" content="telephone=no">
+    <meta name="description" content="Rhythm 5 Fellowship is a non denominational body of believers whos aim is to express christ, live him as life and propagate the scriptural church">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0">
+    <title> Rhythm 5::&coming-soon.html8211; Living Christ as life</title>
+
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Caveat:400,700|Lato:300,300i,400,400i,700,700i|Raleway:300,300i,400,400i,700,700i&amp;subset=latin-ext">
+    <link rel="stylesheet" href="css/fontello/css/fontello.css" type="text/css" media="all" />
+
+    <link rel="stylesheet" href="js/vendor/revslider/css/settings.css" type="text/css" media="all" />
+    <link rel="stylesheet" href="css/plugin.revslider.min.css" type="text/css" media="all" />
+
+    <link rel="stylesheet" href="css/style.css" type="text/css" media="all" />
+    <link rel="stylesheet" href="css/core.animation.min.css" type="text/css" media="all" />
+    <link rel="stylesheet" href="css/shortcodes.min.css" type="text/css" media="all" />
+
+    <link rel="stylesheet" href="css/plugin.instagram-widget.min.css" type="text/css" media="all" />
+
+    <link rel="stylesheet" href="js/vendor/essential-grid/css/settings.css" type="text/css" media="all" />
+    <link rel="stylesheet" href="js/vendor/essential-grid/css/lightbox.css" type="text/css" media="all" />
+    <link rel="stylesheet" href="css/tpl-essential-grids.min.css" type="text/css" media="all" />
+
+    <link rel="stylesheet" href="css/skin.min.css" type="text/css" media="all" />
+    <link rel="stylesheet" href="css/responsive.min.css" type="text/css" media="all" />
+
+    <link rel="stylesheet" href="css/core.messages.min.css" type="text/css" media="all" />
+    <link rel="stylesheet" href="js/vendor/magnific/magnific-popup.min.css" type="text/css" media="all" />
+
+    <link rel="icon" href="images/favicon.png" />
+    <link rel="icon" href="images/favicon.png" />
+    <link rel="apple-touch-icon-precomposed" href="images/favicon.png" />
+</head>
---
+++ resources/views/shared/main/scripts.blade.php
@@ -0,0 +1,35 @@
+<script type="text/javascript" src="js/jquery/jquery.js"></script>
+<script type="text/javascript" src="js/jquery/jquery-migrate.min.js"></script>
+
+<script type="text/javascript" src="js/vendor/photostack/modernizr.min.js"></script>
+<script type="text/javascript" src="js/_main.min.js"></script>
+<script type="text/javascript" src="js/_packed.js"></script>
+
+<script type="text/javascript" src="js/vendor/essential-grid/js/lightbox.js"></script>
+<script type="text/javascript" src="js/vendor/essential-grid/js/jquery.themepunch.tools.min.js"></script>
+<script type="text/javascript" src="js/vendor/essential-grid/js/jquery.themepunch.essential.min.js"></script>
+<script type="text/javascript" src="js/essential-grid-homepage-2.min.js"></script>
+
+<script type="text/javascript" src="js/vendor/revslider/jquery.themepunch.revolution.min.js"></script>
+
+<script type="text/javascript" src="js/vendor/revslider/revolution.extension.slideanims.min.js"></script>
+<script type="text/javascript" src="js/vendor/revslider/revolution.extension.layeranimation.min.js"></script>
+<script type="text/javascript" src="js/vendor/revslider/revolution.extension.kenburn.min.js"></script>
+<script type="text/javascript" src="js/vendor/revslider/revolution.extension.navigation.min.js"></script>
+<script type="text/javascript" src="js/vendor/revslider/revolution.extension.parallax.min.js"></script>
+<script type="text/javascript" src="js/revslider-homepage-2.min.js"></script>
+
+<script type="text/javascript" src="js/superfish.min.js"></script>
+<script type="text/javascript" src="js/core.utils.min.js"></script>
+<script type="text/javascript" src="js/core.init.min.js"></script>
+<script type="text/javascript" src="js/template.init.min.js"></script>
+
+<script type="text/javascript" src="js/shortcodes.min.js"></script>
+<script type="text/javascript" src="js/core.messages.min.js"></script>
+<script type="text/javascript" src="js/vendor/magnific/jquery.magnific-popup.min.js"></script>
+
+<script type="text/javascript" src="js/countdown/jquery.plugin.min.js"></script>
+<script type="text/javascript" src="js/countdown/jquery.countdown.min.js"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.0.1/js/swiper.min.js"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery.isotope/3.0.4/isotope.pkgd.min.js"></script>
+<script type="text/javascript" src="js/jquery/ui/accordion.min.js"></script>
